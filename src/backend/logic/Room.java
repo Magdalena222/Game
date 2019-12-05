daedb91b86dc3df3dcdd6bef806ce3490a9f5232
Maciej Michalak <m.michalak@itkontrakt.com.pl>
2019-12-05 22:10:13 +0100

    Lista pokazuje ladnie pokoje

---
+++ src/backend/logic/Room.java
@@ -0,0 +1,35 @@
+package backend.logic;
+
+public class Room {
+    String name;
+    String player1;
+    String player2;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+}
